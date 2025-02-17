37403f2671f21abf5eaf133bbda95219f470d2bd
Flax420 <rasmus.lillrank42@gmail.com>
2025-02-17 16:09:07 +0100

    Updated Makefile, added wall time and no graphics to galsim

--- Makefile
+++ Makefile
@@ -6,7 +6,17 @@ OBJS = galsim.o graphics/graphics.o
 EXECUTABLE = galsim
 LDFLAGS=-L/opt/X11/lib -lX11 -lm
 
-all:$(EXECUTABLE)
+N=3000
+INPUT=input_data/ellipse_N_03000.gal
+STEPS=100
+DT=0.00001
+GRAPHICS=0
+REF_INPUT=ref_output_data/ellipse_N_03000_after100steps.gal
+
+all:$(EXECUTABLE) compare
+
+compare: compare_gal_files/compare_gal_files.c
+	$(CC) -lm compare_gal_files/compare_gal_files.c -o compare
 
 $(EXECUTABLE): $(OBJS)
 	$(LD) $(LDFLAGS) $(OBJS) -o $(EXECUTABLE)
@@ -17,5 +27,11 @@ graphics/graphics.o: graphics/graphics.c graphics/graphics.h
 galsim.o: galsim.c
 	$(CC) $(CFLAGS) -c galsim.c
 
+test-output: galsim
+	./galsim $(N) $(INPUT) $(STEPS) $(DT) $(GRAPHICS)
+
+check-output: test-output compare
+	./compare $(N) out.gal $(REF_INPUT)
+
 clean:
-	$(RM) $(EXECUTABLE) $(OBJS) && make -C graphics clean
+	$(RM) $(EXECUTABLE) $(OBJS) compare out.gal && make -C graphics clean
--- galsim.c
+++ galsim.c
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
+#include <sys/time.h>
+
 typedef struct vec vec_t;
 typedef struct body body_t;
 
@@ -22,8 +24,15 @@ struct vec {
 
 const float circleColor=0;
 const int windowWidth=800;
-
 const double epsilon = 0.001;
+
+double get_wall_seconds() {
+  struct timeval tv;
+  gettimeofday(&tv, NULL);
+  double seconds = tv.tv_sec + (double)tv.tv_usec / 1000000;
+  return seconds;
+}
+
 body_t* read_file(int N, char* input_name) {
     FILE* file = fopen(input_name, "r");
     if (!file){
@@ -59,12 +68,6 @@ int write_file(int N, body_t* data) {
   fwrite((void*)data, sizeof(body_t), N, file);
   return 0;
 }
-
-/*
-calculate force
-for j
- sum += -gravity * mass[i] * mass[j] * rel_dist(i,j) / (dist(i,j) + epsilon) ** 3
-*/
 
 vec_t force(int j, body_t* bodies, int N){
     
@@ -110,15 +113,25 @@ int main(int argc, char *argv[]) {
     return 0;
   }
   const int N = atoi(argv[1]);
-  printf("N: %i \n", N);
   body_t* bodies = read_file(N, argv[2]);
   const int nsteps = atoi(argv[3]);
   const double dt = atof(argv[4]);
   const int graphics = atoi(argv[5]);
   if(!bodies) {return -1;} // Something went wrong reading the input file
 
-  float L=1, W=1;
+  double time = get_wall_seconds();
+  // Running without graphics
+  if(!graphics) {
+    for(int i = 0; i < nsteps; i++) {
+      update_bodies(bodies, N, dt);
+    }
+    time = get_wall_seconds()-time;
+    printf("No graphics, time = %lf\n", time);
+    write_file(N, bodies);
+    return 0;
+  }
 
+  float L=1, W=1;
   InitializeGraphics(argv[0],windowWidth,windowWidth);
   SetCAxes(0,1);
   printf("Hit q to quit.\n");
@@ -135,8 +148,10 @@ int main(int argc, char *argv[]) {
     /* Sleep a short while to avoid screen flickering. */
     usleep(3000);
   }
-  write_file(N, bodies);
   FlushDisplay();
   CloseDisplay();
+  time = get_wall_seconds() - time;
+  printf("Graphics, time = %lf\n", time);
+  write_file(N, bodies);
   return 0;
 }
